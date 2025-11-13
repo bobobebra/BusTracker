764b25c55c26e654625861dbef488b4f71867dfe
bobobebra <149588933+bobobebra@users.noreply.github.com>
2025-11-13 17:40:37 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// src/App.jsx
+import React from "react";
+import MapView from "./map/MapView.jsx";
+
+export default function App() {
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <MapView />
+    </div>
+  );
+}
